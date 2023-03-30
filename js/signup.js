26beb91125d39c8d72738808c870276d6ae2e78d
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-30 22:26:25 +0530

    signup page - signup.js - ajax request fixed.

--- js/signup.js
+++ js/signup.js
@@ -3,7 +3,16 @@ $(document).ready(function () {
     e.preventDefault();
     if ($(".email").val() != "" && $(".password").val() != "") {
       $.ajax({
-        
+        type: "POST",
+        url: "php/signup.php",
+        data: {
+          email: $(".email").val(),
+          password: $(".password").val(),
+        },
+        cache: false,
+        success: function (response) {
+            alert(response);
+        },
       });
     } else {
       swal("Empty Field!", "Please fill all the Input field!", "warning");

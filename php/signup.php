8f80b17591a237ad181d8eb08590754f7c220520
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-30 22:40:20 +0530

    signup page - signup.php - query processing...

--- php/signup.php
+++ php/signup.php
@@ -1,5 +1,19 @@
 <?php
 
+//DATABASE LINK
 require_once("../common/php/database.php");
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$get_data = "SELECT * FROM signup";
+
+$response = $db -> query($get_data);
+
+if($response){
+    echo "S";
+}else{
+    $
+}
 
 ?>

a97c9331aaed3c81dad1f9e3a191a2055af6f1bb
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-30 23:04:43 +0530

    signup page - signup.php - query fixed.

--- php/signup.php
+++ php/signup.php
@@ -5,13 +5,26 @@ require_once("../common/php/database.php");
 
 $email = $_POST['email'];
 $password = $_POST['password'];
+$otp = rand(212225, 872545);
 
 $get_data = "SELECT * FROM signup";
 
 $response = $db -> query($get_data);
 
 if($response){
-    echo "S";
+   
+    $insert_data = "INSERT INTO signup(email, password, otp) VALUES ('$email', '$password', '$otp')";
+
+        if($db -> query($insert_data)){
+            if(mail($email, "ChatGpt", "Please don't share your OTP with Anyone!", "Your OTP is : ".$otp)){
+                echo "success";
+            }else{
+                "Unable to send OTP!";
+            }
+        }else{
+            echo "Failed";
+        }
+
 }else{
     $create_table = "CREATE TABLE signup(
         id INT(11) NOT NULL AUTO_INCREMENT,
@@ -20,15 +33,19 @@ if($response){
         otp VARCHAR(11),
         status VARCHAR(15) NOT NULL DEFAULT 'PENDING',
         PRIMARY KEY(id)
-    )"
+    )";
 
     if($db -> query($create_table)){
         $insert_data = "INSERT INTO signup(email, password, otp) VALUES ('$email', '$password', '$otp')";
 
         if($db -> query($insert_data)){
-
+            if(mail($email, "ChatGpt", "Please don't share your OTP with Anyone!", "Your OTP is : ".$otp)){
+                echo "success";
+            }else{
+                "Unable to send OTP!";
+            }
         }else{
-            echo "";
+            echo "Failed";
         }
     }else{
         echo "Unable to Create table!";

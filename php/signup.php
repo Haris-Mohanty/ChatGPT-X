2b470d81842eaf2e55bcbbe88a6b6d05d84116e6
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-30 22:41:29 +0530

    signup page - signup.php - Create query processing...

--- php/signup.php
+++ php/signup.php
@@ -13,7 +13,9 @@ $response = $db -> query($get_data);
 if($response){
     echo "S";
 }else{
-    $
+    $create_table = "CREATE TABLE signup(
+        id INT() NOT NULL AUTO_INCREMENT,
+    )"
 }
 
 ?>

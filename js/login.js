59ef8e12d814467935e18153448cc1d25f566d7b
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-01 10:17:15 +0530

    Login page - login button - ajax request fixed.

--- js/login.js
+++ js/login.js
@@ -2,7 +2,18 @@ $(document).ready(function () {
   $(".login-form").submit(function (e) {
     e.preventDefault();
     $.ajax({
-        
+      type: "POST",
+      url: "php/login.php",
+      data: {
+        email: $(".email").val(),
+        password: $(".password").val(),
+      },
+      beforeSend: function () {
+        $(".btn-login").html("Please wait...");
+      },
+      success: function (response) {
+        alert(response);
+      },
     });
   });
 });

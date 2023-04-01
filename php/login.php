b18369931f7d385be2436bfcbd60a0ac12ce94ab
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-01 10:21:28 +0530

    Login page - login.php - query processing...

--- php/login.php
+++ php/login.php
@@ -3,4 +3,12 @@
 //DATABASE LINK
 require_once("../common/php/database.php");
 
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$check_email = "SELECT * FROM signup WHERE email = '$email'";
+
+$response = $db -> query($check_email);
+
+
 ?>

70f19382fc9fe8be3d47217bc51b8f5083f1f30e
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-01 09:40:02 +0530

    signup page - verify - verify.php - query processing...

--- php/verify.php
+++ php/verify.php
@@ -6,4 +6,10 @@ require_once("../common/php/database.php");
 $email = $_POST['email'];
 $otp = $_POST['otp'];
 
+$get_data = "SELECT $otp FROM signup WHERE email = '$email' AND otp = '$otp'";
+
+$response = $db -> query($get_data);
+
+if($response)
+
 ?>

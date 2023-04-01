c59764a8b9b13098e28ea9cdf70bbaa936a50b39
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-01 10:32:17 +0530

    Login page - login.php - email and password check successfully.

--- php/login.php
+++ php/login.php
@@ -6,9 +6,23 @@ require_once("../common/php/database.php");
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$check_email = "SELECT * FROM signup WHERE email = '$email'";
+$check_email = "SELECT email FROM signup WHERE email = '$email'";
 
 $response = $db -> query($check_email);
+if($response -> num_rows != 0){
 
+    $check_pass = "SELECT password FROM signup WHERE email = '$email' AND password = '$password'";
+    $pass_res = $db -> query($check_pass);
+    if($pass_res -> num_rows != 0){
+
+        
+
+    }else{
+        echo "Incorrect Password!":
+    }
+
+}else{
+    echo "Wrong Email!";
+}
 
 ?>

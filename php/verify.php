b6755a849f0b40e5d149b3bada6a68e5f4d3c158
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-01 09:46:33 +0530

    signup page - verify - verify.php - query fixed.

--- php/verify.php
+++ php/verify.php
@@ -10,6 +10,17 @@ $get_data = "SELECT $otp FROM signup WHERE email = '$email' AND otp = '$otp'";
 
 $response = $db -> query($get_data);
 
-if($response)
+if($response -> num_rows != 0){
+
+    $update_status = "UPDATE signup SET status = 'ACTIVE' WHERE email = '$email'";
+    if($db -> query($update_status)){
+        echo "success";
+    }else{
+        echo "Unable to update status!";
+    }
+
+}else{
+    echo "Incorrect OTP!";
+}
 
 ?>

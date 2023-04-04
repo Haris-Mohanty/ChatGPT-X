05d75312249d96d651cfe11ac201008a880a7d67
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-04 20:55:13 +0530

    Dashboard page - dashboard.php - Create table code fixed.

--- php/dashboard.php
+++ php/dashboard.php
@@ -13,7 +13,18 @@ $response = $db -> query($get_data);
 if($response){
 
 }else{
-    $create_table = "CREATE TABLE ";
+    $create_table = "CREATE TABLE dashboard(
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        question VARCHAR(255),
+        answer VARCHAR(255),
+        PRIMARY KEY(id)
+    )";
+
+    if($db -> $($create_table)){
+
+    }else{
+        echo "Unable to Create Table!";
+    }
 }
 
 ?>

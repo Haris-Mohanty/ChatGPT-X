bc73795f0a71d31193b005a95409ae0033c7b64b
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-04 19:34:51 +0530

    Dashboard page - dashboard.php - Create table code processing...

--- php/dashboard.php
+++ php/dashboard.php
@@ -3,4 +3,17 @@
 //DATABASE LINK
 require_once("../common/php/database.php");
 
+$question = $_POST['question'];
+$answer = $_POST['answer'];
+
+$get_data = "SELECT * FROM dashboard";
+
+$response = $db -> query($get_data);
+
+if($response){
+
+}else{
+    $create_table = "CREATE TABLE ";
+}
+
 ?>

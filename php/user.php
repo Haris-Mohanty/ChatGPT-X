b7f42f581183de4ec16c6c1288e00bb7911b8ed6
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-05 21:31:15 +0530

    ChatGpt page- page user.php - query fixed.

--- php/user.php
+++ php/user.php
@@ -5,6 +5,15 @@ require_once("../common/php/database.php");
 
 $question = $_POST['question'];
 
-$get_data = 
+$get_data = "SELECT answer FROM dashboard WHERE question = '$question'";
+
+$response = $db -> query($get_data);
+
+if($response -> num_rows != 0){
+    $data = $response -> fetch_assoc();
+    echo $data;
+}else{
+    echo "Thank You!";
+}
 
 ?>

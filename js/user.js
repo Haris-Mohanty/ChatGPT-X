d36d2f3ed3bf10c0ba94ba9a483354d7291963e5
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-05 21:23:46 +0530

    ChatGpt page- user.js - ajax request fixed.

--- js/user.js
+++ js/user.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
   $(".search-btn").on("click", function () {
     //ajax request
+    let question = $(".user-question").val();
+    let question = $(".user-question").val();
+    $.ajax({
+      type: "POST",
+      url: "php/user.php",
+      data: {
+        question: question,
+      },
+      beforeSend: function () {},
+      success: function (response) {
+        console.log(response.trim());
+      },
+    });
   });
 });
